820f015f1daca955904de4c5d3caae68acf2fe36
hiep.ninh <hiep.ninh@viajsc.local>
2025-04-18 09:41:09 +0700

    update modal for user-history

--- js/user-history.js
+++ js/user-history.js
@@ -1,16 +1,41 @@
 // Filter the news group footer
 document.addEventListener("DOMContentLoaded", function () {
-    const dropdownMenu = document.getElementById("dropdown-list-footer");
-  
-    if (dropdownMenu) {
-      dropdownMenu.addEventListener("click", function (e) {
-        const item = e.target.closest(".dropdown-item");
-        if (item) {
-          const selectedText = item.textContent;
-          document.getElementById("footer-selected-news").textContent =
-            selectedText;
-          document.getElementById("dropdownMenuButton").blur();
-        }
+  const dropdownMenu = document.getElementById("dropdown-list-footer");
+
+  if (dropdownMenu) {
+    dropdownMenu.addEventListener("click", function (e) {
+      const item = e.target.closest(".dropdown-item");
+      if (item) {
+        const selectedText = item.textContent;
+        document.getElementById("footer-selected-news").textContent =
+          selectedText;
+        document.getElementById("dropdownMenuButton").blur();
+      }
+    });
+  }
+});
+
+// open modal 
+document.addEventListener("DOMContentLoaded", function () {
+  const detailContents = document.querySelectorAll(".detail-content");
+  const processModal = new bootstrap.Modal(document.getElementById('processModal'));
+  const modalTitleSpan = document.querySelector("#processModalLabel span");
+  const detailParagraph = document.getElementById("detail");
+
+  detailContents.forEach(detailContent => {
+      detailContent.addEventListener("click", function () {
+          const row = detailContent.closest(".section-dislay-content");
+
+          const nameLabel = row.querySelector("label");
+          const name = nameLabel ? nameLabel.textContent.trim() : "Không rõ";
+
+          const detailText = detailContent.textContent.trim();
+
+          modalTitleSpan.textContent = name + " ";
+
+          detailParagraph.textContent = detailText;
+
+          processModal.show();
       });
-    }
   });
+});
